8b3721365653814fd1385253019356007628d7ff
Paul W. Rankin <rnkn@rnkn.xyz>
2025-07-05 19:46:58 +1000

    lib/Bliptown/Model/User.pm: implemented basic user CRUD

--- lib/Bliptown/Model/User.pm
+++ lib/Bliptown/Model/User.pm
@@ -2,13 +2,62 @@ package Bliptown::Model::User;
 use Mojo::Base -base;
 use Crypt::Bcrypt qw(bcrypt bcrypt_check);
 
-sub create_user { }
-
-sub read_user {
-    my ($self, $username) = @_;
-    my $data = $self->sqlite->db->select('users', undef, { username => $username })->hash;
+sub get_user {
+	my $self = shift;
+	my @hostname = split(/\./, $self->req->url->to_abs->host);
+	return @hostname >= 3 ? $hostname[-3] : 'mayor';
 }
 
-sub update_user { }
+sub create_user {
+	my ($self, $args) = @_;
+	my $password_hash = bcrypt(
+		$args->password, '2b', 12, $ENV{BLIPTOWN_SALT}
+	);
+	$self->sqlite->db->insert(
+		'users', {
+			username => $args->username,
+			email => $args->email,
+			password_hash => $password_hash,
+		});
+}
 
-sub delete_user { }
+sub read_user {
+    my ($self, $args) = @_;
+    my $data = $self->sqlite->db->select(
+		'users', undef, {
+			username => $args->username
+		})->hash;
+}
+
+my @allowed_keys = qw(username email password_hash);
+
+sub update_user {
+	my ($self, $args) = @_;
+	my $values = {};
+	foreach my $key (keys %$args) {
+		if (grep { $key eq $_ } @allowed_keys) {
+			$values->{$key} = %$args{$key};
+		}
+	};
+    $self->sqlite->db->update('users', { $values }, { id => $args->username });
+}
+
+sub delete_user {
+	my ($self, $args) = @_;
+    $self->pg->db->delete(
+		'users', {
+			username => $args->username,
+		});
+}
+
+sub authenticate_user {
+    my ($self, $args) = @_;
+    my $res = $self->read_user({ username => $args->username });
+    my $hash = $res->{password_hash};
+    if ($hash) {
+        return bcrypt_check($args->password, $hash);
+    }
+    return undef;
+}
+
+return 1;
