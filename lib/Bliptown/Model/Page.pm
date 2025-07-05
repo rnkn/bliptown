36c15066cc749e131e4614719a63ec1ec4f9f7f7
Paul W. Rankin <rnkn@rnkn.xyz>
2025-07-05 19:46:28 +1000

    lib/Bliptown/Model/Page.pm: implemented read_page

--- lib/Bliptown/Model/Page.pm
+++ lib/Bliptown/Model/Page.pm
@@ -1,3 +1,38 @@
 package Bliptown::Model::Page;
 use Mojo::Base -base;
+use YAML::Tiny;
+use Text::Markdown;
 
+sub read_page {
+	my ($self, $args) = @_;
+	my $file = $args->{file};
+	return unless -r $file;
+	my $chars = $file->slurp('utf-8');
+
+	my ($yaml, $metadata, $html, $text);
+
+	if ($chars =~ /^(---.*?---)\s*(.*)$/s) {
+		$yaml =$1;
+		$text = $2;
+	} else {
+		$text = $chars;
+	}
+
+	if ($yaml) {
+		my $obj = YAML::Tiny->new;
+		$metadata = $obj->read_string($yaml);
+		$metadata = $metadata->[0];
+	}
+
+	if ($text) {
+		my $obj = Text::Markdown->new;
+		$html = $obj->markdown($text);
+	}
+
+	return {
+		metadata => $metadata,
+		html => $html,
+	};
+}
+
+return 1;
