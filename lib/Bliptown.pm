3dbee209a7752a7598dbef47a728efb44b9b663d
Paul W. Rankin <rnkn@rnkn.xyz>
2025-07-19 15:25:42 +1000

    New helpers, defaults, routes
    /files
    /new/*catchall
    /delete/*catchall

--- lib/Bliptown.pm
+++ lib/Bliptown.pm
@@ -1,7 +1,7 @@
 package Bliptown;
 use Mojo::Base 'Mojolicious';
 use Mojo::SQLite;
-use Mojo::File qw(curfile);
+use Mojo::File qw(curfile path);
 use lib curfile->dirname->sibling('lib')->to_string;
 
 use Bliptown::Model::User;
@@ -40,6 +40,13 @@ sub startup {
 		});
 
 	$app->helper(
+		get_src_dir => sub {
+			state $src = $ENV{'BLIPTOWN_SRC'};
+			return $src;
+		}
+	);
+
+	$app->helper(
 		get_user => sub {
 			my $c = shift;
 			my @hostname = split(/\./, $c->req->url->to_abs->host);
@@ -49,71 +56,60 @@ sub startup {
 	$app->helper(
 		get_file => sub {
 			my ($c, $slug) = @_;
-			my $file_html = $app->home->child($c->get_src_dir, $c->get_user, $slug . '.html');
-			my $file_css = $app->home->child($c->get_src_dir, $c->get_user, $slug . '.css');
-			my $file_md = $app->home->child($c->get_src_dir, $c->get_user, $slug . '.md');
-			return $file_html if -r $file_html;
-			return $file_css if -r $file_css;
-			return $file_md if -r $file_md;
-		}
-	);
-
-	$app->helper(
-		get_home => sub {
-			my $c = shift;
-			state $home = $app->home;
-		}
-	);
-
-	$app->helper(
-		get_src_dir => sub {
-			state $src = $ENV{'BLIPTOWN_SRC'};
-			return $src;
-		}
-	);
-
-	$app->helper(
-		get_pages => sub {
-			my $c = shift;
-			my $paths = $app->home->child($app->get_src, $app->get_user)->list->to_array;
-			my %pages;
-			foreach (@$paths) {
-				$pages{$_} = basename($_);
+			my $root = path($c->get_src_dir, $c->get_user);
+			my @filetypes = qw(html css js txt md);
+			foreach (@filetypes) {
+				my $f = path($root, $slug)->to_abs;
+				return $f if -e $f;
+				$f = path($root, "$slug.$_")->to_abs;
+				return $f if -e $f;
 			}
-			return \%pages;
 		}
+	);
+
+	$app->defaults(
+		head => '',
+		header => '',
+		sidebar => '',
+		footer => '',
+		title => 'Untitled',
+		content => '',
+		ext => 'md',
+		editable => 0,
+		relfile => '',
+		redirect => '/',
+		included => [],
 	);
 
 	my $r = $app->routes;
+
+	$r->get(
+		'/denied' => sub {
+			my $c = shift;
+			$c->render(text => 'Access denied', status => 403)
+		}
+	)->name('access_denied');
 
 	$r->post('/join')->to(controller => 'User', action => 'user_join')->name('user_join');
 	$r->post('/login')->to(controller => 'User', action => 'user_login')->name('user_login');
 	$r->get('/logout')->to(controller => 'User', action => 'user_logout')->name('user_logout');
 
-	$r->get(
-		'/uploads/*catchall' => sub {
-			my $c = shift;
-			my $file = $app->home->child($c->get_src_dir, $c->get_user, 'uploads', $c->param('catchall'));
-			if (-r $file) {
-				return $c->reply->file($file);
-			} else {
-				return $c->reply->not_found;
-			}
-		});
-
 	my $protected = $r->under(
 		'/' => sub {
 			my $c = shift;
-			if ($c->session('username') eq $c->get_user) {
-				$c->stash(auth => 1);
+			if ($c->session('username') && $c->session('username') eq $c->get_user) {
 				return 1;
+			} else {
+				return $c->redirect_to('access_denied');
 			}
-		});
+		}
+	);
 
-	$protected->get('/pages')->to(controller => 'Page', action => 'list_pages')->name('list_pages');
-	$protected->get('/uploads')->to(controller => 'Uploads', action => 'list_uploads')->name('list_uploads');
+	$protected->get('/files')->to(controller => 'Files', action => 'list_files')->name('list_files');
+	$protected->get('/new/*catchall')->to(controller => 'Page', action => 'new_page', catchall => '')->name('new_page');
 	$protected->get('/edit/*catchall')->to(controller => 'Page', action => 'edit_page', catchall => '')->name('edit_page');
 	$protected->post('/edit/*catchall')->to(controller => 'Page', action => 'save_page', catchall => '')->name('save_page');
+	$protected->get('/delete/*catchall')->to(controller => 'Files', action => 'delete_file', catchall => '')->name('delete_file');
 
 	$r->get('/raw/*catchall')->to(controller => 'Page', action => 'render_raw', catchall => '')->name('render_raw');
 	$r->get('/*catchall')->to(controller => 'Page', action => 'render_page', catchall => '')->name('render_page');
