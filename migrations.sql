f2cafa6d5d98faf407d4aa7ac8b4aaf9e17a4605
Paul W. Rankin <rnkn@rnkn.xyz>
2025-11-03 17:10:19 +1000

    Rewrite SQL to use UNIQUE INDEX

--- migrations.sql
+++ migrations.sql
@@ -1,10 +1,18 @@
+-- 2 up
+ALTER TABLE users
+    ADD COLUMN custom_domain TEXT;
+CREATE UNIQUE INDEX custom_domain_index ON users(custom_domain);
+-- 2 down
+ALTER TABLE users
+    DROP COLUMN custom_domain;
 -- 1 up
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY autoincrement,
-    username TEXT UNIQUE NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-	totp_secret TEXT NOT NULL
+	username TEXT,
+    email TEXT,
+    password_hash TEXT,
+	totp_secret TEXT
 );
+CREATE UNIQUE INDEX username_index ON users(username);
+CREATE UNIQUE INDEX email_index ON users(email);
 -- 1 down
-DROP TABLE IF EXISTS users;
+DROP TABLE users;
