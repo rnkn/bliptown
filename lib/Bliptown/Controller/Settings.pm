123c69e1b2cb6db5ea2e2bd8d73489a872d2233e
Paul W. Rankin <rnkn@rnkn.xyz>
2025-11-23 18:17:11 +1000

    Add provision_cert ipc message

--- lib/Bliptown/Controller/Settings.pm
+++ lib/Bliptown/Controller/Settings.pm
@@ -22,8 +22,19 @@ sub list_settings {
 sub save_settings {
 	my $c = shift;
 	my $username = $c->session('username');
+	my $user = $c->user->read_user(
+		{ key => 'username', username => $username }
+	);
+	my $cur_domain = $user->{custom_domain};
 	my $params = $c->req->params->to_hash;
 	$c->user->update_user({ username => $username, %$params });
+	my $new_domain = $params->{custom_domain};
+	if ($cur_domain && $new_domain and $cur_domain ne $new_domain) {
+		$c->ipc->send_message(
+			command => 'provision_cert',
+			payload => { domain => $new_domain },
+		)
+	};
 	return $c->redirect_to($c->url_for('render_page'));
 }
 
