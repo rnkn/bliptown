91a599e6ff54b8ecfe7e4216ad6bc477b8ca51a9
Paul W. Rankin <rnkn@rnkn.xyz>
2025-12-14 21:34:19 +1000

    Add responses to IPC <=> bliptown_helper

--- lib/Bliptown/Model/IPC.pm
+++ lib/Bliptown/Model/IPC.pm
@@ -2,8 +2,12 @@ package Bliptown::Model::IPC;
 use Mojo::Base -base;
 use IO::Socket::UNIX;
 use Sereal::Encoder;
+use Sereal::Decoder;
 
 my $encoder = Sereal::Encoder->new;
+my $decoder = Sereal::Decoder->new;
+
+$SIG{CHLD} = 'IGNORE';
 
 sub send_message {
 	my ($self, $args) = @_;
@@ -16,20 +20,30 @@ sub send_message {
 	my $data = {
 		command => $args->{command},
 		payload => {
-			username => $args->{username},
-			filename => $args->{filename},
-			new_filename => $args->{new_filename} // '',
-			create_backup => $args->{create_backup} // 0,
-			content => $args->{content} // '',
-			blob => $args->{blob} // '',
-			domain => $args->{domain} // '',
-			all_domains => $args->{all_domains} // [],
+			username		=> $args->{username},
+			filename		=> $args->{filename},
+			new_filename	=> $args->{new_filename} // '',
+			create_backup	=> $args->{create_backup} // 0,
+			content			=> $args->{content} // '',
+			blob			=> $args->{blob} // '',
+			domain			=> $args->{domain} // '',
+			all_domains		=> $args->{all_domains} // [],
 		},
 	};
 
-	my $blob = $encoder->encode($data);
-	$client->send($blob) || die "Cannot send data to socket ($!)";
+	my $req_blob = $encoder->encode($data);
+	my $res_blob;
+	$client->send($req_blob) or die "Cannot send data to socket ($!)";
+
+	shutdown($client, 1) or die "Cannot shutdown socket ($!)";
+
+	while (read($client, my $buf, 512)) {
+		$res_blob .= $buf;
+	}
+
 	$client->close;
+	my $res = $decoder->decode($res_blob);
+	return $res;
 }
 
 return 1;
