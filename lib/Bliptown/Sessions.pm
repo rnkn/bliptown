e22f18be0910b8851174e0c50a9e2a05d171482e
Paul W. Rankin <rnkn@rnkn.xyz>
2026-01-15 22:17:11 +1000

    Only set a custom domain cookie_domain

--- lib/Bliptown/Sessions.pm
+++ lib/Bliptown/Sessions.pm
@@ -4,13 +4,10 @@ use Mojo::Base 'Mojolicious::Sessions';
 sub store {
 	my ($self, $c) = @_;
 
-	my $domain = $c->config->{domain};
 	my $custom_domain = $c->stash('custom_domain') // '';
 	$self->cookie_domain(".$custom_domain") if $custom_domain;
-	my $res = $self->SUPER::store($c);
-	$self->cookie_domain($domain);
 
-	return $res;
+	return $self->SUPER::store($c);
 }
 
 return 1;
